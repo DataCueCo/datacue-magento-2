954f22cf7703194b34d0989bc761f3d3d5ff7a7a
chenruimin <ruiminch@163.com>
2019-06-30 17:06:48 +0000

    clear client on uninstalling

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -5,9 +5,15 @@ namespace DataCue\MagentoModule\Setup;
 use Magento\Framework\Setup\UninstallInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\App\ObjectManager;
 
 class Uninstall implements UninstallInterface
 {
+    /**
+     * @var \Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory $collectionFactory
+     */
+    private $collectionFactory;
+
     public function uninstall(
         SchemaSetupInterface $setup,
         ModuleContextInterface $context
@@ -17,6 +23,38 @@ class Uninstall implements UninstallInterface
         if ($setup->getConnection()->isTableExists($tableName)) {
             $setup->getConnection()->dropTable($tableName);
         }
+
+        // clear client
+        $objectManager = ObjectManager::getInstance();
+        $this->collectionFactory = $objectManager->create('Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory');
+        $client = new \DataCue\Client(
+            $this->getApiKey(),
+            $this->getApiSecret(),
+            ['max_try_times' => 3],
+            file_exists(__DIR__ . '/../staging') ? 'development' : 'production'
+        );
+        try {
+            $client->client->clear();
+        } catch (\Exception $e) {
+
+        }
+
         $setup->endSetup();
+    }
+
+    private function getApiKey()
+    {
+        $collection = $this->collectionFactory->create();
+        $items = $collection->addFieldToFilter('path', 'datacue/api_key')->getColumnValues('value');
+
+        return count($items) > 0 ? $items[0] : '';
+    }
+
+    private function getApiSecret()
+    {
+        $collection = $this->collectionFactory->create();
+        $items = $collection->addFieldToFilter('path', 'datacue/api_secret')->getColumnValues('value');
+
+        return count($items) > 0 ? $items[0] : '';
     }
 }
